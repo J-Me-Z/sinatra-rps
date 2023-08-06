1dd0c25dec90ed4ba85fb4e40c446826744a79a4
J-Me-Z <136928731+J-Me-Z@users.noreply.github.com>
2023-08-06 02:15:40 +0000

    still struggle

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require 'sinatra'
 
 get('/') do
   "
+  <body>
+  <div>
+  
+    <div><a href="Play Rock">2d6</a></div>
+    <div><a href="Play Paper">2d10</a></div>
+    <div><a href="Play Scissors">1d20</a></div>
+  </div>
+
+
   <h1>Welcome to Rock-Paper-Scissors!</h1>
   <p>Define some routes in app bla</p>
   "
