59b7532ca3070d3970ed7d0623a25dffe3ab7bab
J-Me-Z <136928731+J-Me-Z@users.noreply.github.com>
2023-08-06 02:52:49 +0000

    small progress

--- app.rb
+++ app.rb
@@ -2,17 +2,68 @@ require 'sinatra'
 
 get('/') do
   "
-  <body>
-  <div>
-  
-    <div><a href="Play Rock">2d6</a></div>
-    <div><a href="Play Paper">2d10</a></div>
-    <div><a href="Play Scissors">1d20</a></div>
-  </div>
-
-
   <h1>Welcome to Rock-Paper-Scissors!</h1>
   <p>Define some routes in app bla</p>
   "
 
+end
+
+
+get("/rock") do
+  options = ["rock", "paper", "scissors"]
+  first_hand= "rock"
+  second_hand =options.sample
+	
+  @outcome = "We played #{first_hand}"
+  @outcome =  "They played #{second_hand}."
+
+if second_hand = "rock"
+   @outcome "We tied!"
+if second_hand = "paper"
+  @outcome "We lost!"
+els @outcome "We won!"
+end
+  #erb(:two_ten, {:layout => :wrapper})
+	
+  # "<h1>2d10</h1>
+  #  <p>#{outcome}</p>"
+end
+get("/paper") do
+  options = ["rock", "paper", "scissors"]
+  first_hand= "paper"
+  second_hand =options.sample
+	
+  @outcome = "We played #{first_hand}"
+  @outcome =  "They played #{second_hand}."
+
+if second_hand = "paper"
+   @outcome "We tied!"
+if second_hand = "scissors"
+  @outcome "We lost!"
+els @outcome "We won!"
+end
+  #erb(:two_ten, {:layout => :wrapper})
+	
+  # "<h1>2d10</h1>
+  #  <p>#{outcome}</p>"
+end
+
+get("/scissors") do
+  options = ["rock", "paper", "scissors"]
+  first_hand= "scissors"
+  second_hand =options.sample
+	
+  @outcome = "We played #{first_hand}"
+  @outcome =  "They played #{second_hand}."
+
+if second_hand = "scissors"
+   @outcome "We tied!"
+if second_hand = "rock"
+  @outcome "We lost!"
+els @outcome "We won!"
+end
+  #erb(:two_ten, {:layout => :wrapper})
+	
+  # "<h1>2d10</h1>
+  #  <p>#{outcome}</p>"
 end
